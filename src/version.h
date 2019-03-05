a9fc541dc0172ac0fa982a2e622e9c2520669d01
Sierracoin Foundation Team <41061929+sierracoin-team@users.noreply.github.com>
2019-03-05 10:37:41 +0300

    Update protocol version

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 10200;
+static const int PROTOCOL_VERSION = 10202;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -23,7 +23,7 @@ static const int GETHEADERS_VERSION = 10000;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 10200;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 10200;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 10202;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
