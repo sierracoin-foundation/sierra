e3de3a02441051c611c08002305a1e5fb4940cdd
Sierracoin Foundation Team <41061929+sierracoin-team@users.noreply.github.com>
2019-07-26 15:43:52 +0300

    Update masternode install script for version v.2.1.2

--- contrib/masternodes/sierra_masternode_install.sh
+++ contrib/masternodes/sierra_masternode_install.sh
@@ -34,7 +34,7 @@ COIN_CLI="${coin_name}-cli"
 TMP_FOLDER=$(mktemp -d)
 COIN_PATH='/usr/local/bin/'
 KERN_ARCH=$(getconf LONG_BIT)
-COIN_TGZ="https://github.com/sierracoin-foundation/sierra/releases/download/v.2.1.0/sierra-2.1.0-linux${KERN_ARCH}.tar.gz"
+COIN_TGZ="https://github.com/sierracoin-foundation/sierra/releases/download/v.2.1.2/sierra-2.1.2-linux${KERN_ARCH}.tar.gz"
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
